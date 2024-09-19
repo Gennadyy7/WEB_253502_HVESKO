328fc4566811a3b516ee30c1a295382ec9287095
Gennady_Hvesko <pprom5573@gmail.com>
2024-09-19 14:02:03 +0300

    Удалил стартовые файлы и папки и создал контроллер Home

---
+++ WEB_253502_HVESKO.UI/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253502_HVESKO.UI.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
