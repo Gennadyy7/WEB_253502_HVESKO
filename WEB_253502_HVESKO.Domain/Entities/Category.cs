c122e0d08e79df02748be8d770481ccc7cbe531b
Gennady_Hvesko <pprom5573@gmail.com>
2024-09-26 21:34:00 +0300

    Создал библиотеку классов и связал с основным проектом

---
+++ WEB_253502_HVESKO.Domain/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WEB_253502_HVESKO.Domain.Entities
+{
+    internal class Category
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        private string normalizedName;
+
+        public string NormalizedName
+        {
+            get => normalizedName;
+            set => normalizedName = ConvertToKebabCase(value);
+        }
+
+        private string ConvertToKebabCase(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return string.Empty;
+
+            return Regex.Replace(str.Trim(), @"\s+", "-").ToLower();
+        }
+    }
+}
---
+++ WEB_253502_HVESKO.Domain/Entities/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_253502_HVESKO.Domain.Entities
+{
+    internal class Service
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string? Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string MimeType { get; set; }
+    }
+}
